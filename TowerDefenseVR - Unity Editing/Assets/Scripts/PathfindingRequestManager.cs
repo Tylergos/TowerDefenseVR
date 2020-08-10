d7837dfffe823757b4ea504b952fe8b606eb827f
Dioji <tyler.gosbee@gmail.com>
2020-08-10 06:38:52 -0300

    Added HackerMan and wall model

---
+++ TowerDefenseVR - Unity Editing/Assets/Scripts/PathfindingRequestManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathfindingRequestManager : MonoBehaviour
+{
+    public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback)
+    {
+        
+    }
+}
